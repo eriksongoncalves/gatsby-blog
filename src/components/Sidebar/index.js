6fab297c8e0e7f4a6e0d333703a70bc2150553b8
Erikson Gonçalves <eriksongoncalves@yahoo.com.br>
2020-09-21 10:47:00 -0300

    feat: add menu link component

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,11 +3,13 @@ import React from 'react'
 import * as S from './styled'
 import Profile from '../Profile'
 import SocialLinks from '../SocialLinks'
+import MenuLinks from '../MenuLinks'
 
 const Sidebar = () => (
   <S.SidebarWrapper>
     <Profile />
     <SocialLinks />
+    <MenuLinks />
   </S.SidebarWrapper>
 )
 
