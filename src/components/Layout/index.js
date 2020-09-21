b18463602ace8c0d045ae24dec1f4e84d9cdab0e
Erikson Gonçalves <eriksongoncalves@yahoo.com.br>
2020-09-21 10:20:13 -0300

    feat: sidebar styles

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,29 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
 
-import Profile from '../Profile'
+import * as S from './styled'
+import Sidebar from '../Sidebar'
 import GlobalStyles from '../../styles/global'
-
-const LayoutWrapper = styled.section`
-  display: flex;
-`
-
-const LayoutMain = styled.main`
-  background: #16202c;
-  min-height: 100vh;
-  padding: 0 3.75 0 20rem;
-  width: 100%;
-`
 
 const Layout = ({ children }) => {
   return (
-    <LayoutWrapper>
+    <S.LayoutWrapper>
       <GlobalStyles />
-      <aside>
-        <Profile />
-      </aside>
-      <LayoutMain> {children}</LayoutMain>
-    </LayoutWrapper>
+      <Sidebar />
+      <S.LayoutMain> {children}</S.LayoutMain>
+    </S.LayoutWrapper>
   )
 }
 
